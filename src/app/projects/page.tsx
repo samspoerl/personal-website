8b8021435c0a5868fb125ef699de80b02af55d95
Sam Spoerl <sam@samspoerl.com>
2024-06-30 12:33:48 -0400

    Update project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,60 +1,107 @@
 import { type Metadata } from 'next'
-import Image from 'next/image'
+import Image, { ImageProps } from 'next/image'
 
+import { GitHubIcon } from '@/components/SocialIcons'
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import screenshot1 from '@/images/photos/personal-website.png'
+import { Button } from '@/components/Button'
+import Link from 'next/link'
 
-const projects = [
+interface Project {
+  name: string
+  description: string
+  gitHubLink?: string
+  demoLink?: string
+  image?: ImageProps['src']
+  imageSubstitute?: string
+}
+
+const projects: Project[] = [
   {
     name: 'This site!',
     description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
+      "The site you're looking at is one of my projects. I built it to introduce myself and showcase my work.",
+    gitHubLink: 'https://github.com/samspoerl/personal-website',
+    image: screenshot1,
   },
   {
-    name: 'Animaginary',
+    name: 'Personal Finance App',
     description:
-      'High performance web animation library, hand-written in optimized WASM.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
+      "I built this app to automate my monthly financial statements. It uses Plaid to fetch balances and transactions in real time. Check out the demo using Plaid's Sandbox environment.",
+    imageSubstitute: 'Coming soon!',
+    // gitHubLink: 'https://github.com/samspoerl/personal-finance-app',
+    // demoLink: 'https://finance.samspoerl.com',
   },
   {
-    name: 'HelioStream',
+    name: 'AI Assistant',
     description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
+      "No, I'm not trying to compete with ChatGPT. I built this app because the free version runs older models and uses your data to train its models. I didn't want to pay for ChatGPT+ and wanted some features they didn't have yet. Plus, it was fun to build.",
+    imageSubstitute: 'In progress',
   },
   {
-    name: 'cosmOS',
+    name: 'Notes App',
     description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
+      "One of my biggest frustrations with nearly all of the note-taking apps on the market is that they all depend on title-based organization. Coming up with titles is burdensome and I feel like it stifles my creativity. I want an note-taking and journaling app that is date-based, so I don't have to create titles.",
+    imageSubstitute: 'Planned',
   },
 ]
 
-function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+function ProjectCard({ project }: { project: Project }) {
   return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
-        fill="currentColor"
-      />
-    </svg>
+    <>
+      <div className="flex h-64 flex-row overflow-hidden rounded-lg bg-white shadow-xl dark:bg-zinc-900 dark:shadow-2xl dark:shadow-zinc-950">
+        <div className="flex w-1/3 items-center justify-center">
+          {project.image !== undefined ? (
+            <Image
+              src={project.image}
+              alt=""
+              className="h-full w-full object-cover"
+              unoptimized
+            />
+          ) : (
+            <p className="text-center text-xl font-medium">
+              {project.imageSubstitute}
+            </p>
+          )}
+        </div>
+        <div className="flex h-full w-2/3 flex-col justify-between p-6">
+          <div>
+            <p className="mb-4 text-base text-lg font-semibold text-zinc-800 dark:text-zinc-100">
+              {project.name}
+            </p>
+            <p className="text-sm text-zinc-600 dark:text-zinc-400">
+              {project.description}
+            </p>
+          </div>
+          <div className="mt-4 flex flex-row items-center justify-end gap-x-4">
+            {project.gitHubLink !== undefined && (
+              <Link
+                href={project.gitHubLink}
+                aria-label="View repository on GitHub"
+              >
+                <GitHubIcon className="h-10 w-10 fill-zinc-800 transition hover:fill-zinc-700 dark:fill-zinc-500 dark:hover:fill-zinc-400" />
+              </Link>
+            )}
+
+            {project.demoLink !== undefined && (
+              <Button href={project.demoLink} variant="primary">
+                Demo
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="size-4"
+                >
+                  <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
+                  <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
+                </svg>
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
@@ -71,44 +118,25 @@ export default function Projects() {
         <>
           <p>
             I’ve worked on tons of little projects over the years but these are
-            the ones that I’m most proud of. Work on side projects for reasons
-            of learning and curating a portfolio of work, but the strongest
-            motivator for me usually that I want the app for myself, either
-            because I want custom features, don't want to pay for another
-            service, or I want more control over my data.
+            the ones that I’m most proud of. I work on side projects to learn
+            and build a portfolio, but the strongest motivator for me is usually
+            that I want the app for myself, either because I want custom
+            features, don't want to pay for another service, or I want more
+            control over my data.
           </p>
           <br />
           <p>
-            Many of them are open-source, so if you see something that piques
-            your interest, check out the code and contribute if you have ideas
-            for how it can be improved."
+            Some of them are a work in progress, some have live demos, and many
+            of them are open-source. If you see something that piques your
+            interest, check out the code and contribute if you have ideas for
+            how it can be improved.
           </p>
         </>
       }
     >
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-      >
-        {projects.map((project) => (
-          <Card as="li" key={project.name}>
-            <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image
-                src={project.logo}
-                alt=""
-                className="h-8 w-8"
-                unoptimized
-              />
-            </div>
-            <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              <Card.Link href={project.link.href}>{project.name}</Card.Link>
-            </h2>
-            <Card.Description>{project.description}</Card.Description>
-            <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-              <LinkIcon className="h-6 w-6 flex-none" />
-              <span className="ml-2">{project.link.label}</span>
-            </p>
-          </Card>
+      <ul role="list" className="mx-auto flex max-w-7xl flex-col gap-y-12">
+        {projects.map((project, i) => (
+          <ProjectCard key={i++} project={project} />
         ))}
       </ul>
     </SimpleLayout>
